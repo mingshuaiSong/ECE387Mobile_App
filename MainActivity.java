1b55f028cb510fb896bb29d317a1947062e5a23a
mingshuaiSong <64703975+mingshuaiSong@users.noreply.github.com>
2022-04-02 21:07:14 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -11,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // the counter
     int cnt = 0;
+    // onClick event to allow user click on button 
+    // , and count the times of clicking on button
     public void onClick(View view) {
         cnt++;
         TextView text = findViewById(R.id.textView);
